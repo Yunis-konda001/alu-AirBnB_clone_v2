6c1cc5f2a6afb997b8b076c94f7b130a5b7a3418
Yunis-Konda001 <k.konda@alustudent.com>
2025-01-22 13:16:08 +0300

    sg

--- models/state.py
+++ models/state.py
@@ -1,8 +1,34 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.city import City
+from os import getenv
+
+storage = getenv("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    if storage == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref='states')
+    else:
+        name = ""
+        cities = []
+    if storage != 'db':
+        @property
+        def cities(self):
+            """
+            return list of cities
+            """
+            from models import storage
+            from models.city import City
+            list_cities = []
+            cities = models.storage.all(City)
+            for key, city_obj in cities.items():
+                if city_obj.state_id == self.id:
+                    list_cities.append(city_obj)
+            return list_cities
