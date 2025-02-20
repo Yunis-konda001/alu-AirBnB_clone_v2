0d9e3ef0d57d3046a68903225b78ecaa47818773
Yunis-Konda001 <k.konda@alustudent.com>
2025-02-20 20:31:27 +0300

    sg

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Bash script that starts a Flask web application
+"""Bash script that starts a Flask web application.
 """
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,27 +9,32 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """ Hello HBNB! """
+    """Hello HBNB!"""
     return "Hello HBNB!"
 
+
 def hbnb():
-    """ HBNB! """
+    """HBNB!"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """Displays 'C' followed by the values
-    replacing underscores with spaces """
+    """Displays 'C' followed by the value of text,
+    replacing underscores with spaces."""
     text = text.replace("_", " ")
-    return 'C{}'.format(text.replace('_', ' '))
+    return "C {}".format(text)
 
+
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is cool'):
-    """ Displays 'Python' followed by the values
-    replacing underscores with spaces """
+def python_is_cool(text="is cool"):
+    """Displays 'Python' followed by the value of text,
+    replacing underscores with spaces."""
     text = text.replace("_", " ")
-    return 'Python {}'.format(text.replace('_', ' '))
+    return "Python {}".format(text)
+
 
 if __name__ == '__main__':
-    """ starts the flask app """
+    """Starts the flask app."""
     app.run(host='0.0.0.0', port=5000)
