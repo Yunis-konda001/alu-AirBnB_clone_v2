09ca2f8459567ae45ac49fcf78de31bc7e604b05
Yunis-Konda001 <k.konda@alustudent.com>
2025-02-20 20:35:03 +0300

    sg

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" Bash script that starts a Flask web application
-"""
+"""Bash script that starts a Flask web application."""
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,33 +7,37 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """ Hello HBNB! """
+    """Hello HBNB!"""
     return "Hello HBNB!"
 
+
 def hbnb():
-    """ HBNB! """
+    """HBNB!"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """Displays 'C' followed by the values
-    replacing underscores with spaces """
+    """Displays 'C' followed by the value of text,
+    replacing underscores with spaces."""
     text = text.replace("_", " ")
-    return 'C{}'.format(text.replace('_', ' '))
+    return "C {}".format(text)
 
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is cool'):
-    """ Displays 'Python' followed by the values
-    replacing underscores with spaces """
+def python_is_cool(text):
+    """Displays 'Python' followed by the value of text,
+    replacing underscores with spaces."""
     text = text.replace("_", " ")
-    return 'Python {}'.format(text.replace('_', ' '))
+    return "Python {}".format(text)
 
-@app.route('/number/<n>', strict_slashes=False)
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def n_is_number(n):
-    """ Displays 'n is a number' only if n is an integer"""
-    n = int(n)
-    return '{} is a number'.format(n)
+    """Displays 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
+
 
 if __name__ == '__main__':
-    """ starts the flask app """
     app.run(host='0.0.0.0', port=5000)
