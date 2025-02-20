785432210e253f9e82665722913848b2ed97e271
Yunis-Konda001 <k.konda@alustudent.com>
2025-02-20 14:48:46 +0300

    sg

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,27 +1,19 @@
 #!/usr/bin/python3
-""" Bash script that starts a Flask web application
-"""
+"""Bash script that starts a Flask web application."""
 from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """ Hello HBNB! """
-    return "HBNB"
-
-
-def hbnb():
-    """ HBNB! """
+    """Display HBNB"""
     return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """Replace underscores with spaces """
+    """Display "C <text>" with underscores replaced by spaces."""
     text = text.replace("_", " ")
-    return 'C{}'.format(text.replace('_', ' '))
+    return "C {}".format(text)
 
 if __name__ == '__main__':
-    """ starts the flask app """
     app.run(host="0.0.0.0", port=5000)
