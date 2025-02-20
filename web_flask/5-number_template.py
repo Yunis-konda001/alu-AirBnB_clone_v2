b0dfb0797c09a9b7e15dafafd027a9e322c1a83e
Yunis-Konda001 <k.konda@alustudent.com>
2025-02-20 20:42:06 +0300

    sg

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,46 +1,43 @@
 #!/usr/bin/python3
-""" Bash script that starts a Flask web application
-"""
-from flask import Flask
+"""Bash script that starts a Flask web application."""
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """ Hello HBNB! """
+    """Displays 'Hello HBNB!'."""
     return "Hello HBNB!"
 
-def hbnb():
-    """ HBNB! """
-    return "HBNB"
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """Displays 'C' followed by the values
-    replacing underscores with spaces """
+    """Displays 'C' followed by the value of text with underscores replaced by spaces."""
     text = text.replace("_", " ")
-    return 'C{}'.format(text.replace('_', ' '))
+    return "C {}".format(text)
 
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is cool'):
-    """ Displays 'Python' followed by the values
-    replacing underscores with spaces """
+def python_is_cool(text):
+    """Displays 'Python' followed by the value of text with underscores replaced by spaces."""
     text = text.replace("_", " ")
-    return 'Python {}'.format(text.replace('_', ' '))
+    return "Python {}".format(text)
 
-@app.route('/number/<n>', strict_slashes=False)
+
+@app.route('/number/<int:n>', strict_slashes=False)
 def n_is_number(n):
-    """ Displays 'n is a number' only if n is an integer"""
-    n = int(n)
-    return '{} is a number'.format(n)
+    """Displays 'n is a number' only if n is an integer."""
+    return "{} is a number".format(n)
 
-@app.route('/number_template/<n>', strict_slashes=False)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-        """Displays a HTML page only if n is an integer"""
-        n = int(n)
-        return render_template('5-number.html', n=n)
+    """Displays an HTML page only if n is an integer."""
+    return render_template('5-number.html', n=n)
+
 
 if __name__ == '__main__':
-    """ starts the flask app """
+    """Starts the Flask web application."""
     app.run(host='0.0.0.0', port=5000)
